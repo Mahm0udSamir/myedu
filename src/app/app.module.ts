78716b5a325e99787796507b69fcccc1dd59e189
Mahm0udSamir <mahmoud.samir.fci.scu@gmail.com>
2023-10-03 22:55:30 +0300

    Init App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SubscribeFormComponent } from './pages/subscribe-form/subscribe-form.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SubscribeFormComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
