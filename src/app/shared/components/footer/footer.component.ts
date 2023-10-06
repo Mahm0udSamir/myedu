0346e530340761dddf745d78a913285eff8e3fb7
Mahm0udSamir <mahmoud.samir.fci.scu@gmail.com>
2023-10-06 14:14:08 +0300

    Add footer component

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IonicModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  links: string[] = [
+    'link One',
+    'link Two',
+    'link Three',
+    'link Four'
+  ]
 }
